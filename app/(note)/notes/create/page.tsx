3049b717f88596378ebbfb398a55e17b683c5a13
jinoc <choiae369@gmail.com>
2025-02-06 13:00:00 +0900

    feat: 노트 작성 페이지 헤더 추가
    - noteList, notes 라우트를 (note)로 분류 -> 폴더 구조 변경

---
+++ app/(note)/notes/create/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import NoteHeader from '../../_components/noteHeader/NoteHeader';
+
+export default function CreateNote() {
+  return (
+    <main className="px-4">
+      <NoteHeader />
+    </main>
+  );
+}
