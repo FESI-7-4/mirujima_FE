816a1c55fe91313403e0140e996845cda1ffb1c3
songa <thddk0121@gmail.com>
2025-02-07 10:09:02 +0900

    feat: todo 필터링 컴포넌트 분리 및 EmptyMessage 컴포넌트 추가

--- app/todoList/page.tsx
+++ app/todoList/page.tsx
@@ -6,21 +6,23 @@ import { useQuery } from '@tanstack/react-query';
 
 import { getTodos } from '@/api/getTodos';
 
-type FilterType = 'All' | 'To do' | 'Done';
+import EmptyMessage from './_components/EmptyMessage';
+import TodoFilter from './_components/TodoFilter';
+
+import type { FilterType } from './_components/TodoFilter';
 
 // 임시 유저 id 값
 const userId = 260;
 
-const filterOptions: FilterType[] = ['All', 'To do', 'Done'];
-
 export default function TodoListPage() {
   const [filter, setFilter] = useState<FilterType>('All');
 
-  const { data } = useQuery({
+  const { data, isLoading, isFetching } = useQuery({
     queryKey: ['todos', userId, filter],
     queryFn: getTodos,
     enabled: !!userId,
-    retry: 0
+    retry: 0,
+    placeholderData: (previousData) => previousData
   });
 
   const filteredTodos = data?.todos.filter((todo) => {
@@ -32,25 +34,22 @@ export default function TodoListPage() {
   return (
     <>
       <div className="flex justify-between">
-        <h2>모든 할 일 ({data?.totalCount})</h2>
+        <h2>모든 할 일 ({data?.totalCount || 0})</h2>
         <button>+ 할일 추가</button>
       </div>
       <div className="mt-4 rounded-xl bg-white p-6 text-black">
-        <ul className="flex gap-2">
-          {filterOptions.map((option) => {
-            return (
-              <li key={option}>
-                <button onClick={() => setFilter(option)}>{option}</button>
-              </li>
-            );
-          })}
-        </ul>
+        <TodoFilter filter={filter} setFilter={setFilter} />
+        {!isLoading && <EmptyMessage filter={filter} filteredTodos={filteredTodos || []} />}
         <div>
-          <ul>
-            {filteredTodos?.map((todo) => {
-              return <li key={todo.id}>{todo.title}</li>;
-            })}
-          </ul>
+          {!isLoading || !isFetching ? (
+            <ul>
+              {filteredTodos?.map((todo) => {
+                return <li key={todo.id}>{todo.title}</li>;
+              })}
+            </ul>
+          ) : (
+            <p>로딩중</p>
+          )}
         </div>
       </div>
     </>
