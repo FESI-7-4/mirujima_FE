2c284e58d950a460e490d45c197fef787b160c3d
songa <thddk0121@gmail.com>
2025-02-10 14:27:26 +0900

    feat: 할 일 항목 상태에 맞는 아이콘 추가

--- app/todoList/_components/TodoItem.tsx
+++ app/todoList/_components/TodoItem.tsx
@@ -6,9 +6,19 @@ interface TodoItemProps {
 
 export default function TodoItem({ todo }: TodoItemProps) {
   return (
-    <li>
-      <input type="checkbox" checked={todo.done} />
-      <span>{todo.title}</span>
+    <li className="group relative flex justify-between">
+      <div>
+        <input type="checkbox" checked={todo.done} />
+        <span>{todo.title}</span>
+      </div>
+      <div className="flex gap-1">
+        {/* TODO: 디자인 확정 후 SVG 아이콘으로 대체  */}
+        {todo.fileUrl && <span>📂</span>}
+        {todo.linkUrl && <span>🔗</span>}
+        {todo.noteId && <span>📄</span>}
+        {!todo.fileUrl && <span className="hidden group-hover:block">✏️</span>}
+        <span className="hidden group-hover:block">⁝</span>
+      </div>
     </li>
   );
 }
