732349ae283321db93715c27f616d690943cd56c
songa <thddk0121@gmail.com>
2025-02-10 16:57:00 +0900

    feat: 할 일 항목에 케밥 UI 추가

--- app/todoList/_components/TodoItem.tsx
+++ app/todoList/_components/TodoItem.tsx
@@ -1,3 +1,5 @@
+import KebabMenu from '../../../components/kebab/KebabMenu';
+
 import type { Todo } from '@/types/todoTypes';
 
 interface TodoItemProps {
@@ -5,19 +7,30 @@ interface TodoItemProps {
 }
 
 export default function TodoItem({ todo }: TodoItemProps) {
+  const handleOpenEditModal = () => {
+    alert('수정하기');
+  };
+  const handleDelete = () => {
+    alert('삭제하기');
+  };
+
   return (
     <li className="group relative flex justify-between">
       <div>
         <input type="checkbox" checked={todo.done} />
         <span>{todo.title}</span>
       </div>
-      <div className="flex gap-1">
+      <div className="relative flex gap-1">
         {/* TODO: 디자인 확정 후 SVG 아이콘으로 대체  */}
         {todo.fileUrl && <span>📂</span>}
         {todo.linkUrl && <span>🔗</span>}
         {todo.noteId && <span>📄</span>}
-        {!todo.fileUrl && <span className="hidden group-hover:block">✏️</span>}
-        <span className="hidden group-hover:block">⁝</span>
+        {!todo.fileUrl && (
+          <button className="hidden group-focus-within:block group-hover:block group-focus:block">
+            ✏️
+          </button>
+        )}
+        <KebabMenu onEdit={handleOpenEditModal} onDelete={handleDelete} />
       </div>
     </li>
   );
