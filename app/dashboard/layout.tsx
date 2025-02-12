6135e60df52a441a315a25e40199c6680c67cdcc
songa <thddk0121@gmail.com>
2025-02-12 16:12:15 +0900

    fix: dashbaord -> dashboard 오타 수정

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function DashbaordLayout({ children }: { children: React.ReactNode }) {
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
