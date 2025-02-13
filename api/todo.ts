fe8fa4007f616de7e2ecb2ff7c368d1222089775
songa <thddk0121@gmail.com>
2025-02-14 00:53:36 +0900

    refactor: pageSize 매개변수 단축 작성

--- api/todo.ts
+++ api/todo.ts
@@ -20,7 +20,7 @@ export const readTodoList = async ({
   else if (filter === 'To do') doneParam = false;
 
   const response = await apiWithClientToken.get<{ result: TodoListType }>('/todos', {
-    params: { lastSeenId: pageParam, pageSize: pageSize, done: doneParam }
+    params: { lastSeenId: pageParam, pageSize, done: doneParam }
   });
   return response.data.result;
 };
