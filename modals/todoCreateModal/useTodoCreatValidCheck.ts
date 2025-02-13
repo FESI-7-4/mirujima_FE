f2651b811017fa2c5de3f3dd35fb23f8f208bbb7
mimi <0506phm@gmail.com>
2025-02-13 10:41:43 +0900

    feat: 유효성 체크 훅 추가

---
+++ modals/todoCreateModal/useTodoCreatValidCheck.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+
+import { useModalStore } from '@/provider/store-provider';
+
+export default function useTodoCreateValidCheck() {
+  const [allValid, setAllValid] = useState<boolean>(false);
+  const { todoCreateModal } = useModalStore((state) => state);
+
+  useEffect(() => {
+    const isFormAllFilled =
+      todoCreateModal.title && todoCreateModal.goal && todoCreateModal.priority ? true : false;
+    setAllValid(isFormAllFilled);
+  }, [todoCreateModal]);
+
+  return { allValid };
+}
