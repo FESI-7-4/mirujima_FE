e7afea8deefdf2bf89e6db86a8ce0c8b8a64a641
songa <thddk0121@gmail.com>
2025-02-14 12:00:02 +0900

    feat: todo 달성률 계산 로직 추가

--- app/dashboard/_components/WeekendChart.tsx
+++ app/dashboard/_components/WeekendChart.tsx
@@ -1,7 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { readTodoProgress } from '@/api/todo';
+
 export default function WeekendChart() {
+  const { data } = useQuery({ queryKey: ['progress'], queryFn: readTodoProgress });
+
+  const completionRate = data?.todoCount
+    ? ((data.completionTodoCount / data.todoCount) * 100).toFixed()
+    : 0;
+
   return (
     <div className="rounded-container w-1/2">
-      <h3 className="h3">이번주 평균 달성률</h3>
+      <h3 className="h3 mb-4">이번주 평균 달성률</h3>
+      <h2 className="mb-6">
+        오늘까지 <span className="text-main">{completionRate}%</span> 달성했어요
+      </h2>
     </div>
   );
 }
