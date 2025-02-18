c614769c4f97a0e0c229858ef5dfac2cdbea0536
jinoc <choiae369@gmail.com>
2025-02-18 15:17:48 +0900

    chore: 불필요한 코드 삭제

--- app/(workspace)/(note)/noteList/@note/(...)notes/[id]/page.tsx
+++ app/(workspace)/(note)/noteList/@note/(...)notes/[id]/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// import NoteContent from '@/app/(note)/_components/noteContent/NoteContent';
 import { redirect } from 'next/navigation';
 
 import { readNoteFromServer } from '@/apis/serverActions/note';
@@ -8,29 +7,15 @@ import ReadOnlyNoteContent from '@/app/(workspace)/(note)/_components/readOnlyNo
 
 import NoteLayoutModal from './modal';
 
-import type { TodoType } from '@/types/todo.type';
-
 interface Props {
   params: Promise<{ id: string }>;
 }
 
 export default async function NoteDetailModal({ params }: Props) {
   const id = (await params).id;
-  const todo: TodoType = {
-    goal: { id: 1, title: '', completionDate: '' },
-    noteId: 5,
-    done: false,
-    linkUrl: '',
-    filePath: '',
-    title: 'string',
-    id: 6,
-    userId: 1,
-    createdAt: '',
-    updatedAt: '',
-    priority: 1
-  };
+
   const note = await readNoteFromServer(Number(id));
-  console.log(note);
+
   if (!note) redirect('/');
 
   return (
