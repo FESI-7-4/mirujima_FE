d82d48626d801e2599fd06a440ebc4dff4a1044c
jinoc <choiae369@gmail.com>
2025-02-18 15:28:33 +0900

    chore: 필요없는 코드 삭제 및 접근성 개선

--- app/(workspace)/(note)/noteList/@note/(...)notes/[id]/modal.tsx
+++ app/(workspace)/(note)/noteList/@note/(...)notes/[id]/modal.tsx
@@ -8,22 +8,15 @@ import { useRouter } from 'next/navigation';
 import BackIcon from '@/public/icon/arrow-left-black.svg';
 
 export default function NoteLayoutModal({ children }: { children: React.ReactNode }) {
-  const dialogRef = React.useRef<HTMLDialogElement>(null);
   const router = useRouter();
 
   const onDismiss = () => {
     router.back();
   };
 
-  React.useEffect(() => {
-    if (!dialogRef.current?.open) {
-      dialogRef.current?.showModal();
-    }
-  }, []);
-
   return createPortal(
     <div className="fixed left-0 top-0 z-50 flex h-screen w-screen justify-end bg-black/50">
-      <div className="w-full bg-white px-6 md:w-2/3 lg:w-3/5">
+      <div role="dialog" aria-label="노트 상세" className="w-full bg-white px-6 md:w-2/3 lg:w-3/5">
         <div className="py-6">
           <button onClick={onDismiss} className="flex-center h-6 w-6" aria-label="뒤로가기">
             <BackIcon />
