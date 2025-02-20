5b17141f5b322618af3c85369ea44d5ae294429f
jinoc <choiae369@gmail.com>
2025-02-20 14:08:30 +0900

    refactor: 노트 관련 모달 리팩토링
    - useModalPortal 추가

--- app/(workspace)/(note)/_components/modals/lodaTempNoteConfirmModal/LoadTempNoteConfirmModal.tsx
+++ app/(workspace)/(note)/_components/modals/lodaTempNoteConfirmModal/LoadTempNoteConfirmModal.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import React from 'react';
+import { createPortal } from 'react-dom';
 
-import ModalLayout from '@/modals/modalLayout/ModalLayout';
+import useModalPortal from '@/hooks/note/useModalPortal';
 
 interface Props {
   tempNoteTitle: string | undefined;
@@ -11,33 +12,44 @@ interface Props {
 }
 
 export default function LoadTempNoteConfirmModal({ tempNoteTitle, onCancle, onConfirm }: Props) {
-  return (
-    <ModalLayout>
-      <div className="space-y-4 text-center">
-        <p className="text-head3 text-gray900">작성중인 글이 있습니다.</p>
-        <div>
-          <p className="text-body1 text-[#636267]">`{tempNoteTitle}`</p>
-          <p className="text-body1 text-[#636267]">제목의 노트를 불러오시겠어요?</p>
-        </div>
-      </div>
-      <div className="mt-6 flex w-full gap-2">
-        <button
-          type="button"
-          onClick={onCancle}
-          aria-label="임시 저장 노트 불러오기 취소"
-          className="h-[40px] w-full rounded-lg border border-main text-button2 text-main md:h-[43px] md:text-button1"
+  const { portal } = useModalPortal();
+
+  return portal
+    ? createPortal(
+        <div
+          role="dialog"
+          aria-modal="true"
+          className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-black/50"
         >
-          취소
-        </button>
-        <button
-          type="button"
-          onClick={onConfirm}
-          aria-label="임시 저장 노트 불러오기"
-          className="h-[40px] w-full rounded-lg bg-main text-button2 text-white md:h-[43px] md:text-button1"
-        >
-          확인
-        </button>
-      </div>
-    </ModalLayout>
-  );
+          <dialog open className="w-11/12 max-w-[450px] rounded-xl bg-white p-6">
+            <div className="space-y-4 text-center">
+              <p className="text-head3 text-gray900">작성중인 글이 있습니다.</p>
+              <div>
+                <p className="text-body1 text-[#636267]">`{tempNoteTitle}`</p>
+                <p className="text-body1 text-[#636267]">제목의 노트를 불러오시겠어요?</p>
+              </div>
+            </div>
+            <div className="mt-6 flex w-full gap-2">
+              <button
+                type="button"
+                onClick={onCancle}
+                aria-label="임시 저장 노트 불러오기 취소"
+                className="h-[40px] w-full rounded-lg border border-main text-button2 text-main md:h-[43px] md:text-button1"
+              >
+                취소
+              </button>
+              <button
+                type="button"
+                onClick={onConfirm}
+                aria-label="임시 저장 노트 불러오기"
+                className="h-[40px] w-full rounded-lg bg-main text-button2 text-white md:h-[43px] md:text-button1"
+              >
+                확인
+              </button>
+            </div>
+          </dialog>
+        </div>,
+        portal
+      )
+    : null;
 }
--- app/(workspace)/(note)/_components/modals/uploadLinkModal/UploadLinkModal.tsx
+++ app/(workspace)/(note)/_components/modals/uploadLinkModal/UploadLinkModal.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import React from 'react';
+import { createPortal } from 'react-dom';
 
-import ModalLayout from '@/modals/modalLayout/ModalLayout';
+import { CloseIcon } from '@/components/icons';
+import useModalPortal from '@/hooks/note/useModalPortal';
+import { useModalStore } from '@/provider/store-provider';
 
 interface Props {
   defaultValue: string | undefined;
@@ -11,30 +14,54 @@ interface Props {
 }
 
 function UploadLinkModal({ defaultValue, onSubmit, linkInputRef }: Props) {
-  return (
-    <ModalLayout title="링크 업로드">
-      <div className="mb-[26px] mt-6">
-        <input
-          ref={(node) => {
-            linkInputRef.current = node;
-            node?.focus();
-          }}
-          defaultValue={defaultValue}
-          onKeyDown={(e) => e.key === 'Enter' && onSubmit()}
-          aria-label="링크 URL 입력"
-          placeholder="링크를 첨부해주세요"
-          className="w-full rounded-lg border border-[#F2EFEF] px-4 py-[10px] outline-none"
-        />
-      </div>
-      <button
-        type="button"
-        onClick={onSubmit}
-        className="h-[50px] w-full rounded-lg bg-main px-4 py-3 font-semibold text-white"
-      >
-        확인
-      </button>
-    </ModalLayout>
-  );
+  const { portal } = useModalPortal();
+  const setNoteLinkModalOpen = useModalStore((store) => store.setNoteLinkModalOpen);
+
+  return portal
+    ? createPortal(
+        <div
+          role="dialog"
+          aria-modal="true"
+          className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-black/50"
+        >
+          <dialog open className="w-11/12 max-w-[450px] rounded-xl bg-white p-6">
+            <div className="flex justify-between">
+              <p className="text-lg font-bold leading-7 text-gray500">링크 업로드</p>
+              <button
+                type="button"
+                name="modal-close-button"
+                aria-label="모달 닫기"
+                onClick={() => setNoteLinkModalOpen(false)}
+              >
+                <CloseIcon />
+              </button>
+            </div>
+
+            <div className="mb-[26px] mt-6">
+              <input
+                ref={(node) => {
+                  linkInputRef.current = node;
+                  node?.focus();
+                }}
+                defaultValue={defaultValue}
+                onKeyDown={(e) => e.key === 'Enter' && onSubmit()}
+                aria-label="링크 URL 입력"
+                placeholder="링크를 첨부해주세요"
+                className="w-full rounded-lg border border-[#F2EFEF] px-4 py-[10px] outline-none"
+              />
+            </div>
+            <button
+              type="button"
+              onClick={onSubmit}
+              className="h-[50px] w-full rounded-lg bg-main px-4 py-3 font-semibold text-white"
+            >
+              확인
+            </button>
+          </dialog>
+        </div>,
+        portal
+      )
+    : null;
 }
 
 export default UploadLinkModal;
