233f4589537f1d9c189d0effa4f47df37089837e
mimi <0506phm@gmail.com>
2025-02-20 14:31:47 +0900

    feat: 메타데이터 및 브레드크럼블 추가

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+export const generateMetadata = () => {
+  return {
+    title: `대시보드`
+  };
+};
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
--- components/navBar/SmallNav.tsx
+++ components/navBar/SmallNav.tsx
@@ -1,7 +1,8 @@
-import type { Dispatch, SetStateAction } from 'react';
+import { type Dispatch, type SetStateAction, useEffect, useState } from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import Info from './Info';
 import Menus from './Menus';
@@ -15,6 +16,13 @@ export default function SmallNav({
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 }) {
+  const path = usePathname();
+  const [title, setTitle] = useState('');
+  useEffect(() => {
+    if (document.title.includes('404')) setTitle('');
+    else setTitle(document.title);
+  }, [path]);
+
   const Inner = () => {
     return (
       <div className="absolute left-0 top-0 z-10 h-screen w-screen translate-x-0 transform flex-col bg-white p-[12px_16px] transition-all duration-300">
@@ -40,7 +48,7 @@ export default function SmallNav({
 
   return (
     <>
-      <div className="absolute left-0 top-0 z-10 flex h-[48px] w-screen gap-[16px] bg-main p-[12px_16px] shadow-md">
+      <div className="item-center absolute left-0 top-0 flex h-[48px] w-screen gap-[16px] bg-main p-[12px_16px] text-white shadow-md">
         <button
           onClick={() => {
             setIsOpen((x) => !x);
@@ -48,6 +56,7 @@ export default function SmallNav({
         >
           <RightArrow />
         </button>
+        <p> {title}</p>
       </div>
 
       <div
