1628474032d63544a12002e3cd9cbb43825ef9db
jinoc <choiae369@gmail.com>
2025-02-26 14:46:17 +0900

    refactor: readOnlyNoteContent에 useNoteActions 사용

--- app/(workspace)/(note)/_components/readOnlyNoteContent/ReadOnlyNoteContent.tsx
+++ app/(workspace)/(note)/_components/readOnlyNoteContent/ReadOnlyNoteContent.tsx
@@ -2,10 +2,8 @@
 
 import React from 'react';
 
-import { useRouter } from 'next/navigation';
-
 import KebabMenu from '@/components/kebab/KebabMenu';
-import useDeleteNote from '@/hooks/note/useDeleteNote';
+import useNoteActions from '@/hooks/note/useNoteActions';
 
 import { Editor } from '../noteContent/editor/DynamicEditor';
 import GoalAndTodoInfo from '../noteContent/goalAndTodoInfo/GoalAndTodoInfo';
@@ -18,16 +16,7 @@ interface Props {
 }
 
 export default function ReadOnlyNoteContent({ note }: Props) {
-  const router = useRouter();
-
-  const { mutate } = useDeleteNote(note.goalDto.id);
-
-  const onEdit = () => {
-    router.push(`/notes/create/${note.todoDto.id}`);
-  };
-  const onDelete = () => {
-    mutate(note.id);
-  };
+  const { onClickEdit, onClickDelete } = useNoteActions(note.goalDto.id);
 
   return (
     <section className="bg-white">
@@ -40,7 +29,11 @@ export default function ReadOnlyNoteContent({ note }: Props) {
       <div className="space-y-2">
         <div className="mt-6 flex gap-[10px] border-y border-gray200 px-4 py-4">
           <h3 className="w-full text-[22px] font-semibold leading-[28px]">{note.title}</h3>
-          <KebabMenu onEdit={onEdit} onDelete={onDelete} size={24} />
+          <KebabMenu
+            onEdit={onClickEdit(note.todoDto.id)}
+            onDelete={onClickDelete(note.id)}
+            size={24}
+          />
         </div>
 
         <div className="space-y-4 px-4 py-[40px]">
