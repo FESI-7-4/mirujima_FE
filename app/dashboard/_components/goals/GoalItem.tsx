c702b8593138649bdde643c17113adc1dbc1bba5
songa <thddk0121@gmail.com>
2025-02-28 02:27:31 +0900

    feat: 대시보드 목표별 할 일에 할일 추가 버튼 추가

--- app/dashboard/_components/goals/GoalItem.tsx
+++ app/dashboard/_components/goals/GoalItem.tsx
@@ -6,7 +6,8 @@ import { useRouter } from 'next/navigation';
 
 import TaskList from '@/components/TaskList/TaskList';
 import { useGetGoalDetail } from '@/hooks/goalsDetail/useGetGoalDetail';
-import { useModalStore } from '@/provider/store-provider';
+import { useModalStore, useTodoCreateModalStore } from '@/provider/store-provider';
+import PlusIcon from '@/public/icon/plus-border-none.svg';
 
 import GoalProgressBar from './GoalProgressBar';
 
@@ -19,9 +20,12 @@ interface GoalItemProps {
 }
 
 export default function GoalItem({ goalId, title, todos }: GoalItemProps) {
+  const setIsTodoCreateModalOpen = useModalStore((state) => state.setIsTodoCreateModalOpen);
+  const setCreatedTodoState = useTodoCreateModalStore((state) => state.setCreatedTodoState);
+
   const { data, isLoading, isError } = useGetGoalDetail(goalId.toString());
-  const { setIsTodoCreateModalOpen } = useModalStore((state) => state);
   const router = useRouter();
+
   const { ref, inView } = useInView({
     triggerOnce: true,
     threshold: 0.3
@@ -48,45 +52,58 @@ export default function GoalItem({ goalId, title, todos }: GoalItemProps) {
     );
   }
 
+  const handleAddTodo = () => {
+    if (goalId) setCreatedTodoState({ goal: { id: goalId } });
+    setIsTodoCreateModalOpen(true);
+  };
+
   return (
-    <Link href={`/goals/${goalId}`}>
-      <div
-        className="rounded-container mb-4 w-full cursor-pointer p-6"
-        onClick={handleContainerClick}
-        ref={ref}
-      >
-        <div className="flex justify-between">
-          <h3 className="truncate text-lg font-bold">{title}</h3>
-        </div>
+    <div className="relative">
+      <Link href={`/goals/${goalId}`}>
+        <div
+          className="rounded-container mb-4 w-full cursor-pointer p-6"
+          onClick={handleContainerClick}
+          ref={ref}
+        >
+          <div className="flex justify-between">
+            <h3 className="truncate pr-16 text-lg font-semibold">{title}</h3>
+          </div>
 
-        <GoalProgressBar todos={todos || []} goalId={goalId} startAnimation={inView} />
+          <GoalProgressBar todos={todos || []} goalId={goalId} startAnimation={inView} />
 
-        <div className="mt-3 flex flex-col border-none desktop:flex-row">
-          <div className="flex-1 overflow-y-auto">
-            <h2 className="z-5 sticky top-0 bg-white py-2 text-[15px] font-medium leading-[20px] text-gray500">
-              To do
-            </h2>
-            <div onClick={(e) => e.stopPropagation()}>
-              <TaskList goalId={goalId} done={false} />
+          <div className="mt-3 flex flex-col border-none desktop:flex-row">
+            <div className="flex-1 overflow-y-auto">
+              <h2 className="z-5 sticky top-0 bg-white py-2 text-[15px] font-medium leading-[20px] text-gray500">
+                To do
+              </h2>
+              <div onClick={(e) => e.stopPropagation()}>
+                <TaskList goalId={goalId} done={false} />
+              </div>
+            </div>
+
+            <hr className="my-4 border-t border-dashed border-gray200 desktop:hidden" />
+
+            <div className="mx-6 my-4 hidden translate-y-5 items-center justify-center desktop:flex">
+              <span className="min-h-[160px] w-px border-l border-dashed border-gray200"></span>
+            </div>
+
+            <div className="flex-1 overflow-y-auto">
+              <h2 className="z-5 sticky top-0 bg-white py-2 text-[15px] font-medium leading-[20px] text-gray500">
+                Done
+              </h2>
+              <div onClick={(e) => e.stopPropagation()}>
+                <TaskList goalId={goalId} done={true} />
+              </div>
             </div>
           </div>
-
-          <hr className="my-4 border-t border-dashed border-gray200 desktop:hidden" />
-
-          <div className="mx-6 my-4 hidden translate-y-5 items-center justify-center desktop:flex">
-            <span className="min-h-[160px] w-px border-l border-dashed border-gray200"></span>
-          </div>
-
-          <div className="flex-1 overflow-y-auto">
-            <h2 className="z-5 sticky top-0 bg-white py-2 text-[15px] font-medium leading-[20px] text-gray500">
-              Done
-            </h2>
-            <div onClick={(e) => e.stopPropagation()}>
-              <TaskList goalId={goalId} done={true} />
-            </div>
-          </div>
         </div>
+      </Link>
+
+      <div className="absolute right-6 top-6 z-10 text-xs">
+        <button onClick={handleAddTodo} className="flex-center text-main">
+          <PlusIcon /> 할일 추가
+        </button>
       </div>
-    </Link>
+    </div>
   );
 }
