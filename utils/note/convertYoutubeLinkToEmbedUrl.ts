31b3333096e4c610a2fbd62def7235c631a0f6d9
jinoc <choiae369@gmail.com>
2025-02-27 17:39:55 +0900

    feat: 노트의 링크가 유튜브일 때 임베드 링크로 변환
    - utils/note/convertYoutubeLinkToEmbedUrl 함수 추가

---
+++ utils/note/convertYoutubeLinkToEmbedUrl.ts
@@ -0,0 +1,26 @@
+const YOUTUBE = 'www.youtube.com';
+const SHORT_YOUTUBE = 'youtu.be';
+const prefix = 'https://' + YOUTUBE + '/embed/';
+
+const convertYoutubeLinkToEmbedUrl = (link: string | undefined) => {
+  if (!link) return undefined;
+
+  try {
+    const url = new URL(link);
+    let videoId = '';
+
+    if (url.hostname === YOUTUBE && url.searchParams.has('v')) {
+      videoId = url.searchParams.get('v') as string;
+      return prefix + videoId;
+    } else if (url.hostname === SHORT_YOUTUBE) {
+      videoId = url.pathname.slice(1);
+      return prefix + videoId;
+    }
+
+    return link;
+  } catch (error) {
+    return '';
+  }
+};
+
+export default convertYoutubeLinkToEmbedUrl;
