f81364925a010d8dad6f5d0fbce0bdcaccfbf810
jinoc <choiae369@gmail.com>
2025-03-06 21:35:31 +0900

    style: 생성한 목표가 없을 때 스타일 변경

--- app/(workspace)/(note)/noteList/page.tsx
+++ app/(workspace)/(note)/noteList/page.tsx
@@ -17,15 +17,18 @@ export default async function AllNoteList() {
         <NoteIcon width="24" height="24" />
         <h2>모든 노트 모아보기</h2>
       </div>
-      <NoGoalNoteList />
+
       {showErrorMessage ? (
-        <section className="flex h-[50px] w-full flex-col gap-2 pl-8 desktop:flex-row">
-          <p className="text-main">생성한 목표가 없습니다!</p>
-          <p>사이드바에서 목표를 추가해보세요!</p>
+        <section className="pl-8">
+          <div className="flex w-full flex-col items-center gap-2 rounded-2xl border border-gray200 bg-white p-6 desktop:flex-row">
+            <p>생성한 목표가 없습니다!</p>
+            <p className="text-main">사이드바에서 목표를 추가해보세요!</p>
+          </div>
         </section>
       ) : (
         goalList.goals.map((goal) => <GoalNoteList key={goal.createdAt} goal={goal} />)
       )}
+      <NoGoalNoteList />
     </section>
   );
 }
