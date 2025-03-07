7be058bf1c605c5469ee759ec28af18196eb8413
mimi <0506phm@gmail.com>
2025-03-07 10:27:00 +0900

    feat: 로그인 -> 대시보드 이동시 로딩 추가
    * 로딩 오버레이 수정

--- modals/Overlay.tsx
+++ modals/Overlay.tsx
@@ -19,9 +19,7 @@ export default function Overlay({ children, onClick }: OverlayProps) {
       onClick={onClick}
       className="absolute top-0 z-50 flex h-full w-full animate-fadeIn items-center justify-center bg-gray-800 bg-opacity-50"
     >
-      <div className="transform animate-modalContent rounded-lg bg-white p-6 opacity-0 shadow-lg">
-        {children}
-      </div>
+      {children}
     </dialog>
   );
 }
