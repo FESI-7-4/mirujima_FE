3a8c83ba042fe6c22807afdcc3cbbe754855a5a7
mimi <0506phm@gmail.com>
2025-03-07 13:28:04 +0900

    refactor: 화면측정훅에 height도 포함

--- hooks/nav/useResize.ts
+++ hooks/nav/useResize.ts
@@ -3,10 +3,16 @@ import { useEffect, useState } from 'react';
 import { debounce } from 'lodash';
 
 export default function useResize() {
-  const [screenSize, setScreenSize] = useState<number>(1920);
+  const [screenSize, setScreenSize] = useState({
+    width: window ? window.innerWidth : 1920,
+    height: window ? window.innerHeight : 1020
+  });
 
   useEffect(() => {
-    const handleResize = debounce(() => setScreenSize(window.innerWidth), 250);
+    const handleResize = debounce(
+      () => setScreenSize({ width: window.innerWidth, height: window.innerHeight }),
+      250
+    );
 
     handleResize();
     window.addEventListener('resize', handleResize);
