73952639d0c00365a8c8196ad4c445731a7d3ce0
mimi <0506phm@gmail.com>
2025-03-10 18:02:53 +0900

    chore: 코드래빗 개선안 반영

--- components/PomodoroTimer/useIsClick.ts
+++ components/PomodoroTimer/useIsClick.ts
@@ -1,5 +1,7 @@
 import { Dispatch, SetStateAction, useState } from 'react';
 
+const CLICK_SECONDS = 200;
+
 export default function useIsClick(setIsExpanded: Dispatch<SetStateAction<boolean>>) {
   const [clickStartTime, setClickStartTime] = useState(0);
   const handleMouseDown = () => {
@@ -8,7 +10,7 @@ export default function useIsClick(setIsExpanded: Dispatch<SetStateAction<boolea
 
   const handleMouseUp = () => {
     const clickDuration = Date.now() - clickStartTime;
-    if (clickDuration <= 200) setIsExpanded((prev) => !prev);
+    if (clickDuration <= CLICK_SECONDS) setIsExpanded((prev) => !prev);
   };
 
   return { handleMouseDown, handleMouseUp };
