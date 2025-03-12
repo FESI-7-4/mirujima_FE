fdce9e75516abef90dc21344cd32c677122dac34
songa <thddk0121@gmail.com>
2025-03-12 11:40:14 +0900

    fix: 암호화 시 랜덤 iv 생성하도록 수정

--- utils/cryptoUtils.ts
+++ utils/cryptoUtils.ts
@@ -2,21 +2,26 @@ import CryptoJS from 'crypto-js';
 
 // 암호화 키
 const SECRET_KEY = process.env.NEXT_PUBLIC_AES_SECRET_KEY;
-const IV = process.env.NEXT_PUBLIC_IV;
 
-if (!SECRET_KEY || !IV) {
-  throw new Error('SECRET_KEY 또는 IV가 설정되지 않았습니다!');
+if (!SECRET_KEY) {
+  throw new Error('SECRET_KEY가 설정되지 않았습니다!');
 }
+
+const key = CryptoJS.enc.Utf8.parse(SECRET_KEY);
 
 // 비밀번호 암호화
 export const encrypt = (pwd: string) => {
   try {
-    const cipher = CryptoJS.AES.encrypt(pwd, CryptoJS.enc.Utf8.parse(SECRET_KEY), {
-      iv: CryptoJS.enc.Utf8.parse(IV),
+    // 랜덤 IV 생성
+    const iv = CryptoJS.lib.WordArray.random(16);
+
+    const cipher = CryptoJS.AES.encrypt(pwd, key, {
+      iv,
       padding: CryptoJS.pad.Pkcs7,
       mode: CryptoJS.mode.CBC
     });
-    return cipher.toString();
+
+    return iv.toString(CryptoJS.enc.Hex) + ':' + cipher.toString();
   } catch (e) {
     console.error('Encryption error occur: ', e);
     return null;
