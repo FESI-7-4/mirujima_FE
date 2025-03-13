e95e28cfb2dfd4ef24799d75a1156c2bb70a4b43
mimi <0506phm@gmail.com>
2025-03-13 16:04:43 +0900

    refactor: 할일체크 훅 관련 쿼리키 todo, done 리팩토링

--- hooks/goalsDetail/useCheckTodoStatus.ts
+++ hooks/goalsDetail/useCheckTodoStatus.ts
@@ -13,19 +13,6 @@ export const useCheckTodo = () => {
   const userId = useInfoStore((state) => state.userId);
   const queryClient = useQueryClient();
 
-  const cacheUpdate = async (queryKey: any[], todo: TodoType) => {
-    await queryClient.setQueryData(queryKey, (cache: cacheType | []) => {
-      if (!cache || Array.isArray(cache)) return [];
-      const oldTodos = cache.pages[0].todos;
-      const newTodos = oldTodos.map((item: TodoType) => {
-        if (item?.id === todo?.id) return todo;
-        else return item;
-      });
-
-      return { ...cache, pages: [{ ...cache.pages[0], todos: newTodos }] };
-    });
-  };
-
   return useMutation({
     mutationFn: async ({ todo }: CheckTodoParams) => {
       if (!todo.id) throw new Error('ToDo id가 없습니다.');
@@ -40,9 +27,40 @@ export const useCheckTodo = () => {
       return { todo };
     },
     onSuccess: async ({ todo }: { todo: TodoType }) => {
-      if (todo.goal) cacheUpdate(['todos', todo.goal.id, userId], todo);
+      if (todo.goal) {
+        await queryClient.setQueryData(
+          ['todos', todo.goal.id, userId, todo.done],
+          (cache: TodoType[]) => {
+            const oldTodos = [...cache];
+            const newTodos = [...oldTodos, todo].sort((a, b) => a.id - b.id);
 
-      cacheUpdate(['allTodos', userId], todo);
+            console.log(cache, oldTodos, newTodos);
+
+            return newTodos;
+          }
+        );
+
+        await queryClient.setQueryData(
+          ['todos', todo.goal.id, userId, !todo.done],
+          (cache: TodoType[]) => {
+            const oldTodos = [...cache];
+            const newTodos = oldTodos.filter((item: TodoType) => item.id !== todo.id);
+
+            return newTodos;
+          }
+        );
+      }
+
+      await queryClient.setQueryData(['allTodos', userId], (cache: cacheType | []) => {
+        if (!cache || Array.isArray(cache)) return [];
+        const oldTodos = cache.pages[0].todos;
+        const newTodos = oldTodos.map((item: TodoType) => {
+          if (item?.id === todo?.id) return todo;
+          else return item;
+        });
+
+        return { ...cache, pages: [{ ...cache.pages[0], todos: newTodos }] };
+      });
     },
     onError: (error, _) => {
       console.error(error);
