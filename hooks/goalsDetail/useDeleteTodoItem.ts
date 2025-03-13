398784c636bea023cac89b61a683b3ea5ed81f16
mimi <0506phm@gmail.com>
2025-03-13 14:09:12 +0900

    refactor: 삭제관련 쿼리 로컬 캐시로 리팩토링

--- hooks/goalsDetail/useDeleteTodoItem.ts
+++ hooks/goalsDetail/useDeleteTodoItem.ts
@@ -5,37 +5,40 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { apiWithClientToken } from '@/apis/clientActions/index';
 import { TODO_DELETE_ERROR, TODO_DELETE_SUCCESS } from '@/constant/toastText';
 import { useInfoStore, useModalStore } from '@/provider/store-provider';
+import { TodoType } from '@/types/todo.type';
+import { cacheType } from '@/types/query.type';
 
-const deleteTodoItem = async (todoId: number): Promise<void> => {
-  await apiWithClientToken.delete(`/todos/${todoId}`);
-};
-
-export function useDeleteTodoItem(goalId?: number) {
+export function useDeleteTodoItem() {
   const userId = useInfoStore((state) => state.userId);
   const queryClient = useQueryClient();
   const setIsLoading = useModalStore((state) => state.setIsLoading);
 
-  return useMutation({
-    mutationFn: (todoId: number) => deleteTodoItem(todoId),
-    onMutate: () => {
-      setIsLoading(true);
-    },
-    onSuccess: (_, todoId) => {
-      setIsLoading(false);
-      queryClient.invalidateQueries({ queryKey: ['allTodos', userId], refetchType: 'all' });
-      queryClient.refetchQueries({ queryKey: ['allTodos', userId] });
-      queryClient.invalidateQueries({
-        queryKey: ['todos', goalId ?? 0, userId],
-        refetchType: 'all'
-      });
-      queryClient.invalidateQueries({
-        queryKey: ['notes', goalId ?? 0, userId],
-        refetchType: 'all'
-      });
+  const cacheUpdate = async (queryKey: any[], todo: TodoType) => {
+    await queryClient.setQueryData(queryKey, (cache: cacheType | []) => {
+      if (!cache || Array.isArray(cache)) return [];
+      const oldTodos = cache.pages[0].todos;
+      const newTodos = oldTodos.filter((item: TodoType) => item.id !== todo.id);
 
+      return { ...cache, pages: [{ ...cache.pages[0], todos: newTodos }] };
+    });
+  };
+
+  return useMutation({
+    mutationFn: async (todo: TodoType) => {
+      await apiWithClientToken.delete(`/todos/${todo.id}`);
+      return { todo };
+    },
+    onMutate: (todo) => {
+      return { todo };
+    },
+    onSuccess: ({ todo }: { todo: TodoType }) => {
+      setIsLoading(false);
+      if (todo.goal) cacheUpdate(['todos', todo.goal.id, userId], todo);
+
+      cacheUpdate(['allTodos', userId], todo);
       toast.success(TODO_DELETE_SUCCESS);
     },
-    onError: () => {
+    onError: (_, _todoId) => {
       setIsLoading(false);
       toast.error(TODO_DELETE_ERROR);
     }
